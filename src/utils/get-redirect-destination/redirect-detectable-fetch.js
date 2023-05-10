eedfb914f4626e2116909c5af1861e8041371161
Level222 <124219660+Level222@users.noreply.github.com>
2023-05-11 00:11:44 +0900

    fetchよりもリダイレクト取得が先に解決するバグを修正

--- src/utils/get-redirect-destination/redirect-detectable-fetch.js
+++ src/utils/get-redirect-destination/redirect-detectable-fetch.js
@@ -1,10 +1,18 @@
 import { getNextFetchRedirectDestination } from "./get-next-fetch-redirect-destination";
 import { promiseAnyValue } from "@utils/promise-any-value";
 
+const forbidRedirectFetch = async (url) => {
+  const response = await fetch(url);
+  if (response.redirected) {
+    throw new Error("Redirect is not allowed.");
+  }
+  return response;
+};
+
 export const redirectDetectableFetch = async (url) => {
   const [key, promiseResult] = await promiseAnyValue(new Map([
     ["gettingRedirectDestination", getNextFetchRedirectDestination(url)],
-    ["fetching", fetch(url)]
+    ["fetching", forbidRedirectFetch(url)]
   ]));
 
   if (key === "gettingRedirectDestination") {
