a9947dda9a9da6427846d15d9ea6e63a819be673
Level222 <124219660+Level222@users.noreply.github.com>
2023-05-11 10:04:21 +0900

    fetchのsignalでリクエストをキャンセル

--- src/utils/get-redirect-destination/redirect-detectable-fetch.js
+++ src/utils/get-redirect-destination/redirect-detectable-fetch.js
@@ -1,8 +1,8 @@
 import { getNextFetchRedirectDestination } from "./get-next-fetch-redirect-destination";
 import { promiseAnyValue } from "@utils/promise-any-value";
 
-const forbidRedirectFetch = async (url) => {
-  const response = await fetch(url);
+const forbidRedirectFetch = async (url, options) => {
+  const response = await fetch(url, options);
   if (response.redirected) {
     throw new Error("Redirect is not allowed.");
   }
@@ -10,12 +10,15 @@ const forbidRedirectFetch = async (url) => {
 };
 
 export const redirectDetectableFetch = async (url) => {
+  const controller = new AbortController();
+
   const [key, promiseResult] = await promiseAnyValue(new Map([
     ["gettingRedirectDestination", getNextFetchRedirectDestination(url)],
-    ["fetching", forbidRedirectFetch(url)]
+    ["fetching", forbidRedirectFetch(url, { signal: controller.signal })]
   ]));
 
   if (key === "gettingRedirectDestination") {
+    controller.abort();
     const redirectUrl = promiseResult;
     return {
       redirected: true, redirectUrl
